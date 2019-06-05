73af0f8cb1fa29bfae75342b4480dba1e5790a69
Adam Dipinto <adam.dipinto@gmail.com>
2019-06-05 07:18:33 -0700

    Imported BrowserRouter in order to setup Router, Routes, and Swith. Register and Login components were imported and included as routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../src/components/layout/Navbar";
 import Landing from "../src/components/layout/Landing";
 import "./App.css";
+import Register from "../src/components/auth/Register";
+import "./App.css";
+import Login from "../src/components/auth/Login";
+import "./App.css";
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
