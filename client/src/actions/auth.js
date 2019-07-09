9148c289e5d2d4cfa2542b40a3642c4b799fafb3
Adam Dipinto <adam.dipinto@gmail.com>
2019-07-08 20:03:31 -0700

    imported user load and auth error from types. Created function for load user, hwere local storage is checked for token. Check api/auth for user if fails provides error

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,22 +1,52 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
+
+// Load User
+export const loadUser = () => async dispatch => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get("http://localhost:5000/api/auth");
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
   const config = {
     headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "x",
-      "Access-Control-Allow-Headers":
-        "Origin, X-Requested-With, Content-Type, Accept"
+      "Content-Type": "application/json"
+      // "Access-Control-Allow-Origin": "x",
+      // "Access-Control-Allow-Headers":
+      //   "Origin, X-Requested-With, Content-Type, Accept"
     }
   };
 
   const body = JSON.stringify({ name, email, password });
 
   try {
-    const res = await axios.post("/api/users", body, config);
+    const res = await axios.post(
+      "http://localhost:5000/api/users",
+      body,
+      config
+    );
 
     dispatch({
       type: REGISTER_SUCCESS,
